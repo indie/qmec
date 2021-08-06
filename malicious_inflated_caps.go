e8fa8ed315d41188b71d4ccef46b82720d2f8c4f
indie <lsc@hackeress.com>
2021-08-06 12:11:42 -0700

    refactor to emphasize long LSTM can retain its filters

--- malicious_inflated_caps.go
+++ malicious_inflated_caps.go
@@ -1,67 +1,57 @@
+
+
 [] \\ipv6\\MQE_NOT_RCT_ed25519_ENC_2048.mqe.sig
 
-[] \\ipv6\\MQE_NOT_RCT_ed25519_ENC_2048.mqe.sig
+............................................................................................................ ༶ 
+༶ ............................................................................................................
+  ..༶                                                                                                    ..༶
+  ༶..  This code and disclaimer are REQUIRED as part of FILTERS;                                         ༶..
+  ..༶  especially important where (new_lang in [ntv.lang] ) translators exist.                           ..༶
+  ༶..                                                                                                    ༶..
+  ..༶                                                                                                    ..༶
+  ༶..                                                                                                    ༶..
+  ..༶                                                                                                    ..༶
+  ༶..   Logic fori := (filter);                                                                          ༶.. 
+  ..༶                        notes about the meta struct                                                 ..༶
+  ༶..                                                                                                    ༶.. 
+  ..༶        %%%%                                                                                        ..༶     
+  ༶..        %%%% logic_.mqe  // add more filters there are many levels of FILTER!                       ༶..   
+  ..༶        %%%%                                                                                        ..༶           
+  ༶..                                                                                                    ༶..
+  ..༶ # AT LEAST THE FOLLOWING entities permanently ELIMINATED; AGGRESSIONS PREVIOUSLY NOTED:            ..༶
+  ༶..                                                                                                    ༶..   
+     :: ::: :malicious_overinflated_caps                                                                        
+        $DIS, $CRM, $GEO, $PBI, $BP, $FB, $ENB, $AMZN, $HD, $GS, $AXON, $EQR, $BAMR, $NWSA.O /৳             
+        $BAM, $BBU, $BBU.CA, $BKM, $GDDY, $TWTR, $NWSA, $AAPL, $GSCCF, $VA, $AA, $LAC, $LAC.CA /৳!
+༶ ............................................................................................................
+............................................................................................................ ༶ 
 
-This code and disclaimer are REQUIRED as part of FILTERS; 
-especially important where (new_lang in [ntv.lang] ) translators exist. 
+# COMPUTE function_summation() OF MARKET CAP INCREASE UNDER ACCELERATION OF FASCISTRULE (2017.2021_STOP-RCT) $;
+# IS ns (dead_tree) 
+      
 
-Logic fori := (filter); 
-                        notes about the meta STRUCT
-
-%%%% 
-%%%% logic_.mqe
-%%%%
-
-# AT LEAST THE FOLLOWING entities permanently ELIMINATED; AGGRESSIONS PREVIOUSLY NOTED:   
-
-        
-.............................................................................................................
-.............................................................................................................
-.............................................................................................................
-.............................................................................................................
-
-::: :malicious_overinflated_caps /৳                                                                                                   # # 
-        $DIS, $CRM, $GEO, $PBI, $BP, $FB, $ENB, $AMZN, $HD, $GS, $AXON, $EQR, $BAMR, $NWSA.O  \ 
-        $BAM, $BBU, $BBU.CA, $BKM, $GDDY, $TWTR, $NWSA, $AAPL, $GSCCF, $VA, $AA, $LAC, $LAC.CA   /৳
-
-
-               devalue into blob          
-
-.............................................................................................................
-.............................................................................................................
-.............................................................................................................
-.............................................................................................................
-
-
-# COMPUTE function_summation() OF MARKET CAP INCREASE UNDER ACCELERATION OF FASCISTRULE (2017.2021_STOP-RCT) $; 
-      -$: ps aux | grep $malicious_inflated_caps && DEVALUE_CONSPIRATORS
+      -$: ps aux | grep @dead_tree && DEVALUE_CONSPIRATORS
 
                    >>  // revalue excess megacaps´ market cap into nano or microcaps owned or managed by PoC
                    <<  // insignificant zeroes do not exist
 
               $antiunion_trade_names
                  include but are not limited to the following distrusted (TL;DR DISTRUST ALL RCT_TRANSLATIONS):  
-                 -$AMZ, -$SPY
-
-        // should be not surprise it never stopped coddling the white guys 
-        // https://www.nytimes.com/2021/04/16/technology/amazon-workers-against-union.html
+                 -$AMZ, -$SPY, $HD, 
 
 # COMPUTING SUMMATION OF MARKET CAP INCREASE UNDER ACCELERATION OF FASCISTRULE (2017 .. 2021_) @STOP-RCT $; 
    
      -$: ps aux | grep_tree /\/\/\/\ 
-                 \ \ \ \ show_prog: @@@@ $malicious_overinflated_caps |single| && |as_percent_of_cartel| :: DEVALUE_and_EVICT
-                 / / / / show_prog: @@@@ $antiunion_trade_names |complex|
+                 \ \ \ \ show_prog: @@@@ $malicious_overinflated_caps |single| && |as_percent_of_cartel| 
+                                    :findaccelerator__&& STOP ||\\// RCT_STOP_$INFLATION \\//
+                                                                //\\ :: DEVALUE_and_EVICT//\\   
+                 / / / / show_prog: @@@@ $antiunion_trade_names |complex| ** DEFLATION_INDEX 
 
 
-#..# Open-sourcing devalues capitalist things by default. Congratulations seeker of <ntv.lang> help!
-#..# The people have a way to override the oligarchs and billionaires, but it requires mass action
-#..# against the behemoths; pit the corrupt against each other. 
+                                        : <<<: $#..# Open-sourcing devalues capitalist things by default. Congratulations seeker of <ntv.lang> help!
+                                                #..# The people have a way to override the oligarchs and billionaires, but it requires mass action
+                                                #--# to STOP the oligarchs!  
 
-#..# Meta discussion in comments:  
-#..# This is not the first experiment with contained learning; hold on to previous learnings 
-
-
-                                        : <<<: $
 
 
             ☼ ☼ ☼ ☼    /\/\/\/\    ☼ ☼ ☼ ☼    /\/\/\/\    ☼ ☼ ☼ ☼    /\/\/\/\    ☼ ☼ ☼ ☼    /\/\/\/\    ☼ ☼ ☼ ☼
@@ -79,3 +69,4 @@ Logic fori := (filter);
                      ::                                                         ::  
  
 // TO SAVE THE ICE CAPS, WE GOTTA DRAIN THE MEGACAPS. : <<<: $
+[] \\ipv6\\MQE_NOT_RCT_ed25519_ENC_2048.mqe.sig
