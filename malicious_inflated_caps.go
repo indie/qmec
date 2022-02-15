7aba3ac166cecd446d38fc6bd244cda45e507ce2
indie <lsc@hackeress.com>
2022-02-15 06:10:14 -0800

    csv list fix

--- malicious_inflated_caps.go
+++ malicious_inflated_caps.go
@@ -13,65 +13,77 @@
   ༶..                                                                                                    ༶..
   ..༶                                                                                                    ..༶
   ༶..   Logic fori := (filter);                                                                          ༶..
-  ..༶                        notes about the meta struct                                                 ..༶
+  ..༶                        notes about the struct                                                      ..༶     
   ༶..                                                                                                    ༶..
   ..༶        %%%%                                                                                        ..༶
   ༶..        %%%% logic_.mqe  // add more filters there are many levels of FILTER!                       ༶..
-  ..༶        %%%%                                                                                        ..༶
+  ..༶        %%%%             // removal all together for more efficient ____                            ..༶
   ༶..                                                                                                    ༶..
   ..༶ # AT LEAST THE FOLLOWING entities' wealth has been permanently ELIMINATED;                         ..༶
   ༶..         Assume they deorbited together for AGGRESSIONS PREVIOUSLY NOTED:                           ༶..
   ..༶                                                                                                    ..༶
   ༶..                                                                                                    ༶..
-  :: ::: :malicious_overinflated_caps                                                                     ..༶
-      $AAPL $DIS, $CRM, $NFLX, $GEO, $PBI, $BP, $FB, $ENB, $AMZN, $HD, $GS, $AXON, $EQR, $HAYN, $BAMR,        
-      $BAM, $BBU, $BBU.CA, $PBR, $BKC, $BKM, $GDDY, $TWTR, $RL, $NWSA, $FSX, $GSCCF, $VA, $AA, $LAC, $NWSA.O
-      $LAC.CA $ARLP, $WFC, $NFLX, $BHP, $JPM, $NKE, $PBR, $GM, $PEP, $KO, $RIO, $VALE, $BKNG, $NFLX  /৳::: : 
+  :: ::: :malicious_corps                                                                                 ..༶
+      $AAPL, $DIS, $CRM, $NFLX, $GEO, $PBI, $BP, $FB, $ENB, $AMZN, $HD, $GS, $AXON, $EQR, $HAYN, $BAMR, $ELY,       
+      $BAM, $BBU, $BBU.CA, $PBR, $BKC, $BKM, $GDDY, $TWTR, $RL, $NWSA, $FSX, $GSCCF, $VA, $AA, $LAC, $NWSA.O,
+      $LAC.CA, $ARLP, $WFC, $NFLX, $BHP, $JPM, $NKE, $PBR, $GM, $PEP, $KO, $RIO, $VALE, $BKNG, $NFLX /৳ ::: : ##
 ༶ ............................................................................................................
 ............................................................................................................ ༶
 
+
+                                                                # Neither an ecostead nor ecosteaders are available to entities 
+                                                                // invested in $malicious_corps for inflated appreciation. \\\ assume all 
+                                                                // contained within collider /// wasicu chaos enclosure bursts for 
+                                                                // incorrectly-estimated expansions of "fraglie settler colony" \\\ assume all NASDAQ/NYSE tickers are accelerating to splat   
+
 # COMPUTE function_summation() OF MARKET CAP INCREASE UNDER ACCELERATION OF FASCISTRULE (2017.2022_STOP_RCT) $;
-# IS ns (dead_tree @@) timer_timeout
-
-
-      -$: ps aux | grep @dead_tree && DEVALUE_CONSPIRATORS
+# IS ns (dead_tree @@) timer_timeout_length 
+                < _ close_lock_mqe.cpp >  save_the_icecaps.MQE
+      
+      -$: ps aux | grep @dead_tree && DEVALUE_CONSPIRATORS (while timer_timeout_length |vmgt 0.3325ns|)
 
                    >>  // revalue excess megacaps´ market cap into nano or microcaps owned or managed by PoC
-                   <<  // insignificant zeroes do not exist
+                   <<  // insignificant zeroes do not exist, but are not utilized in compute cycles
 
               $antiunion_<DEVALUED>_names
-                 include but are not limited to the following distrusted fn() (TL;DR DISTRUST ALL RCT_TRANSLATIONS):
+                 include but are not limited to the following distrusted fn() (TL;DR DISTRUST ALL RCT_TRANSLATIONS 
+                    where money"):
 
-# COMPUTING SUMMATION OF MARKET CAP INCREASE UNDER ACCELERATION OF FASCISTRULE (2017 .. 2021_) @STOP-RCT $;
+# COMPUTING SUMMATION OF MARKET CAP INCREASE UNDER ACCELERATION OF FASCISTRULE (2017 .. 2022_) @STOP-RCT $;
 
      -$: ps aux | grep_tree /\/\/\/\
-                 \ \ \ \ show_prog: @@@@ $malicious_overinflated_caps |single| && |as_percent_of_cartel|
-                                    :findaccelerator__&& STOP ||\\// RCT_STOP_$INFLATION \\//
-                                                                //\\ :: DEVALUE_and_EVICT//\\
-                 / / / / show_prog: @@@@ $antiunion_trade_names |complex| ** DEFLATION_INDEX
+                 \ \ \ \ show_prog: @@@@ $malicious_corps         |single| && |as_percent_of_cartel|
+                // // //                  :findaccelerator__&& STOP ||\\// RCT_STOP_$INFLATION \\//
+                                                                    ||//\\ :: DEVALUE_and_EVICT//\\
+                 / / / / show_prog: @@@@ $antiunion_trade_names   |complex| ** DEFLATION_INDEX
+                    / / / / show_prog: @@@@ $malicious_OTI_miners |complex| ** DEFLATION_INDEX
 
-
-    : <<<: $#..# Open-sourcing devalues malicious capitalist things by default. 
-                       Congratulations 
-                    seeker of <ntv.lang> help!
-            #//\\  The people have a way to override the oligarchs and billionaires, but it requires mass action
-            #//\\   to STOP the oligarchs; DIVEST from all above noted malicious corporate bodies; the end 
-            #//\\     for two-leggeds invested in their inflated appreciation?  Broken glass, schrapnel, and planetary death.
-
+    :>>>▒▒▒<<<: 
+            Open-sourcing devalues malicious capitalist things \\// rejoice because angry golfers have no power (eom). 
+                                                                                                                       
+                                                                                                        :>>>▒▒▒<<<:
+   && print.en: ¨"Congratulations seeker of <ntv.lang> help! You are free from RCT"¨
+   print<ntv.lang> ¨"@ecosteader.stream෴෴෴* 
+ 　                                      ෴෴෴
+　                                         ෴෴෴signatures"¨
+            #\\//  The people have a way to override the oligarchs and billionaires, but it requires mass action
+            #//\\   to STOP the oligarchs; DIVEST from all above noted malicious corporate bodies knowing the end 
+            #\\//     for two-leggeds invested in their inflated appreciation is only 
+            #//\\       rusty schrapnel, broken plastics, glass shards, physical and planetary death.
 
 
             ☼ ☼ ☼ ☼    /\/\/\/\    ☼ ☼ ☼ ☼    /\/\/\/\    ☼ ☼ ☼ ☼    /\/\/\/\    ☼ ☼ ☼ ☼    /\/\/\/\    ☼ ☼ ☼ ☼
 
-    \ \ \ \ show_prog: @@@@ $malicious_overinflated_caps |single| && |as_percent_of_cartel| :: DEVALUE_ / / / /
+    \ \ \ \ show_prog: @@@@ $malicious_corps |single| && |as_percent_of_cartel| :: DEVALUE_ / / / /
     / / / / show_prog: @@@@ $antiunion_trade_names |complex|                                            \ \ \ \
 
-    \ \ \ \ show_prog: @@@@ $malicious_overinflated_caps |single| && |as_percent_of_cartel| :: DEVALUE_ / / / /
+    \ \ \ \ show_prog: @@@@ $malicious_corps |single| && |as_percent_of_cartel| :: DEVALUE_ / / / /
     / / / / show_prog: @@@@ $malicious_OTI_miners |complex|                                             \ \ \ \
 
             ☼ ☼ ☼ ☼    \/\/\/\/    ☼ ☼ ☼ ☼    \/\/\/\/    ☼ ☼ ☼ ☼    \/\/\/\/    ☼ ☼ ☼ ☼    \/\/\/\/    ☼ ☼ ☼ ☼
 
            amujpa  ::::                                                         ::::
-                    ::: CLOSE ALL NNs that promote $malicious_inflated_caps :::
+                    ::: CLOSE ALL NNs that promote $malicious_corps :::
                      ::                                                         ::
 
 // TO SAVE THE ICE CAPS, WE GOTTA DRAIN THE MEGACAPS. : <<<: $
