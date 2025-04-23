c72fe15a0930bdc3974543b2bb5f9037996d5e64
Leona C. <info@ecosteader.com>
2025-04-23 07:56:02 -0700

    the more BTC hoarded by the "he" denoted in the .mqe ...
    The bigger the tornadoes and lightning strikes upon his datacenters

--- CNNC.go
+++ CNNC.go
@@ -3,26 +3,269 @@ __
     /// If E´e:  [[  ]]
     ¨¨" Gisgaja'tuap gutapsunnu, smtug maita'nej "¨¨
     
-     go get kittyprogrammer_keyboard.sym  && 
-     
-     clone git@github.com:indie/kittyprogrammer.git
+#  WELCOME TO THE PUBLIC INTERNET'S CHIEF INFORMATION OFFICER, INDIE FROM ECOSTEADER. SHE IS ALSO THE SAME INDIE ON GITHUB
+   AND THESE CODES WORK AROUND THE WWW ANYWHERE YOU GO HTTPS OR NOT HTTPS. 
+ # HTTPS MAKES IT EASIER TO TRACK THE WEALTHY PEOPLE'S FINANCIAL INTEREST, AS IT IS POSSIBLE TO TRACK DOWN THE BANKING PARTNERS'
+   INTERESTS OF THE CONGLOMERATES
+ # PUBLIC INTERNET DOES NOT ACCEPT CLOUDFLARE IN ANY SECURE CONNECTION AT OR WHERE IT IS 
+   ATTEMPTING TO ASSERT THAT CLOUDFLARE IS WHAT MAKES THE INTERNET CONNECTION "SECURE" -- THE INTERNET IS MOST SECURE WITHOUT CLOUDFLARE,
+   WHICH PERFORMS THE SAME FUNCTION AS SPYWARE / MALWARE AND IS WHY FASCISTS LIKE IT. HELLO GOOGLE, THIS IS WHY THE INDIES ARE BRINGING 
+   THIS TO YOUR ATTENTION.
 
-     @capture C++.go style_formatting.nGraph.(.mqe)
-     @capture .css style_render:ecosteader_api (++)
-                              < >      < >                                   
-          < >      < >--------< >     < >                    
-          < >--------< >      < >    filter ----\ ¨Retain Share Previous Learnings¨  includes a hefty DISALLOW screen.
-                                                   DISALLOW_ALL ( iterate again ¨we do not trust any nodes enabling colonizer social media¨ 
-                    ✫                                              --> returnto_RCT_RCT )
-                     \                  -->> Educate, do not indoctrinate! https://kolektiva.social/web/statuses/106699477028971784
-                   /  \\                             
-                  /    \\\  ✫  
-                //             /\                         -->> "Do not mess with the natives' architecture".                            
+/// Do nonRCT SSH keys already exist?  RETRANSLATE into 3 more indigenous languages
+__
+    /// If E´e:  [[  ]]
+    ¨¨" Gisgaja'tuap gutapsunnu, smtug maita'nej "¨¨
+    
+                     \                  
+                   /  \\
+                 /    \\\  ✫  
+                //             /\                                                
                 \\\     //   //                                    
-                  \\   /     \\  /       
-                    \         \\/  ✫                             / receive programmatic sifter ✫.mqe IF AND ONLY IF  .. 
-                     ✫                          CLOSE_LOCK.*: && :: :  SUCCESS      
-                                           Ϡ 
+                  \\   /     \\  /           
+                    \         \\/  ✫          
+                     ✫                             
+                                           
+                   
+
+      ' ¨Retain Share Previous Learnings¨ '  
+         includes this priceless KNOWLEDGE :: ::: 
+                        from the BEGINNING :: ::: ::: 
+                                                         "For each _milliliter_ of jet fuels 
+                                                                         any CEO of ANY [[malicious_inflated_cap.mqe]] did go && use, 
+                                                                          ++ ANYWHERE he did or does jet-set himself 
+                                                                                            into an imitation of "success" 
+                                                                                            into the falsehood of "importance", 
+                                                                          the sharpest microblade (invisible to the naked eye) 
+                                                                                   persistently twists in his nervous system; 
+                                                                            wherefore ~~~   HE DOES YIELD MAXIMUM SPIRITUAL SUFFERING, 
+                                                                                            AND EARNS ONLY TRAGEDY UPON HIS GENETIC SPAWN." 
+
+            space is big and empty for a reason -- to perpetuate the defeat of man: 
+                                                   https://www.moomoo.com/community/feed/how-much-silicon-valley-is-bad-at-counting-and-accounting-114110379851781?share_code=03cznK
+
+                                                                            "LET THE RECORDS SHOW THE FALSE CHIEFS' DEFEAT IS PERMANENT, 
+                                                                                     hereforth identified for all electric currents now and forever, 
+                                                                                            CEOs of Incorporated Meta shall NEVER know joy. "   
+                    ✫                                              --> return_to_RCT with suffering in TIME equal to VOLUME of MILLILITERS of jet fuels  \
+                                                                        that sneering CEO burned flying around the planet. 
+                                                                                                     " )
+                     \                  
+                   /  \\                                         {{ << malicious_inflated_caps.mqe >> }}. GO
+                  /    \\\  ✫  
+                //             /\                                                
+                \\\     //   //                                    
+                  \\   /     \\  /           
+                    \         \\/  ✫          
+                     ✫                             
+                                           
+
+                                      filter---- \\ condense nnq notes on the mqe // 
+
+{ 
+   Free_excess_memory
+
+   //     \ close all ports pulling cat toward any schedule of RCT 
+                                                   //  
+   \\ Collapse &&
+            Extinguish_All_Disallowed } !  
+                                                   // __> Quantum energy cannot be captured by colonizers; 
+             
+                                                                                                        circuitry whiskers not whispers
+            < /// >    ҉      ҉   < \\\ >
+           < /// > < /// >  < \\\ > < \\\ >
+          < /// > < /// >    < \\\ > < \\\ >
+
+          < \\\ > < \\\ >    < /// > < /// >
+           < \\\ > < \\\ >  < /// > < /// >
+            < \\\ >    ҉      ҉   < /// >
+
+            < /// >                < \\\ >
+           < /// > < /// >  < \\\ > < \\\ >
+          < /// > < /// >    < \\\ > < \\\ >
+                          ҉
+          < \\\ > < \\\ >    < /// > < /// >
+           < \\\ > < \\\ >  < /// > < /// >
+            < \\\ >                < /// >
+
+            < /// >               < \\\ >
+           < /// > < /// > < \\\ > < \\\ >
+          < /// > < /// >   < \\\ > < \\\ >
+          < \\\ > < \\\ >   < /// > < /// >
+           < \\\ > < \\\ > < /// > < /// >
+            < \\\ >               < /// >
+
+              < /// > < // \\ > < \\\ >
+             < /// > < ======= > < \\\ >
+            < /// >               < \\\ >
+            < \\\ >               < /// >
+             < \\\ > < ======= > < /// >
+              < \\\ > < \\ // > < /// >
+              < /// > < // \\ > < \\\ >
+             < /// > < ======= > < \\\ >
+            < /// >               < \\\ >
+            < \\\ >               < /// >
+             < \\\ > < ======= > < /// >
+              < \\\ > < \\ // > < /// >
+              < /// > < // \\ > < \\\ >
+             < /// > < ======= > < \\\ >
+            < /// >               < \\\ >
+            < \\\ >               < /// >
+             < \\\ > < ======= > < /// >
+              < \\\ > < \\ // > < /// >
+              < /// > < // \\ > < \\\ >
+             < /// > < ======= > < \\\ >
+            < /// >               < \\\ >
+            < \\\ >               < /// >
+             < \\\ > < ======= > < /// >
+              < \\\ > < \\ // > < /// >
+              < /// > < // \\ > < \\\ >
+             < /// > < ======= > < \\\ >
+            < /// >               < \\\ >
+            < \\\ >               < /// >
+             < \\\ > < ======= > < /// >
+              < \\\ > < \\ // > < /// >
+
+                   < \\\ > < /// >
+                   < /// > < \\\ >
+                  < /// >   < \\\ >
+                 < /// >     < \\\ >
+                 < \\\ >     < /// >
+                  < \\\ >   < /// >
+                   < \\\ > < /// >
+                   < /// > < \\\ >
+            < /// >               < \\\ >
+           < /// > < /// > < \\\ > < \\\ >
+          < /// > < /// >   < \\\ > < \\\ >
+          < \\\ > < \\\ >   < /// > < /// >
+           < \\\ > < \\\ > < /// > < /// >
+            < \\\ >               < /// >
+
+              < /// > < // \\ > < \\\ >
+             < /// > < ======= > < \\\ >
+            < /// >               < \\\ >
+            < \\\ >               < /// >
+             < \\\ > < ======= > < /// >
+              < \\\ > < \\ // > < /// >
+                   < /// > < \\\ >
+                  < /// >   < \\\ >
+                 < /// >     < \\\ >
+
+                < /// >       < \\\ >
+                 ecosteader
+              < /// > < // \\ > < \\\ >
+             < /// > < ======= > < \\\ >
+            < /// >               < \\\ >
+           < /// > < /// > < \\\ > < \\\ >
+          < /// > < /// >   < \\\ > < \\\ >
+
+         < /// < < < /// > < \\\ > > > \\\ >
+        < /// < < < /// >   < \\\ > > > \\\ >
+       < /// < < < /// >     < \\\ > > > \\\ >
+       < \\\ < < < \\\ >     < /// > > > /// >
+        < \\\ < < < \\\ >   < /// > > > /// >
+         < \\\ < < < \\\ > < /// > > > /// >
+
+          < \\\ > < \\\ >   < /// > < /// >
+           < \\\ > < \\\ > < /// > < /// >
+            < \\\ >               < /// >
+             < \\\ > < ======= > < /// >
+              < \\\ > < \\ // > < /// >
+                           mechq mesh__________ ::: [graphene_wire] @indie´s authorship;
+                < \\\ >       < /// >               she work too hard on details relevant to quantum slip :::
+
+                 < \\\ >     < /// >
+                  < \\\ >   < /// >
+                   < \\\ > < /// >
+              < /// > < // \\ > < \\\ >
+             < /// > < ======= > < \\\ >
+            < /// >               < \\\ >
+            < \\\ >               < /// >
+             < \\\ > < ======= > < /// >
+              < \\\ > < \\ // > < /// >
+
+            < /// >               < \\\ >
+           < /// > < /// > < \\\ > < \\\ >
+          < /// > < /// >   < \\\ > < \\\ >
+          < \\\ > < \\\ >   < /// > < /// >
+           < \\\ > < \\\ > < /// > < /// >
+            < \\\ >               < /// >
+                   < \\\ > < /// >
+                   < /// > < \\\ >
+                  < /// >   < \\\ >
+                 < /// >     < \\\ >
+                 < \\\ >     < /// >
+                  < \\\ >   < /// >
+                   < \\\ > < /// >
+                   < /// > < \\\ >
+
+              < /// > < // \\ > < \\\ >
+             < /// > < ======= > < \\\ >
+            < /// >               < \\\ >
+            < \\\ >               < /// >
+             < \\\ > < ======= > < /// >
+              < \\\ > < \\ // > < /// >
+              < /// > < // \\ > < \\\ >
+             < /// > < ======= > < \\\ >
+            < /// >               < \\\ >
+            < \\\ >               < /// >
+             < \\\ > < ======= > < /// >
+              < \\\ > < \\ // > < /// >
+              < /// > < // \\ > < \\\ >
+             < /// > < ======= > < \\\ >
+            < /// >               < \\\ >
+            < \\\ >               < /// >
+             < \\\ > < ======= > < /// >
+              < \\\ > < \\ // > < /// >
+              < /// > < // \\ > < \\\ >
+             < /// > < ======= > < \\\ >
+            < /// >               < \\\ >
+            < \\\ >               < /// >
+             < \\\ > < ======= > < /// >
+              < \\\ > < \\ // > < /// >
+              < /// > < // \\ > < \\\ >
+             < /// > < ======= > < \\\ >
+            < /// >               < \\\ >
+            < \\\ >               < /// >
+             < \\\ > < ======= > < /// >
+              < \\\ > < \\ // > < /// >
+
+
+            < /// >               < \\\ >
+           < /// > < /// > < \\\ > < \\\ >
+          < /// > < /// >   < \\\ > < \\\ >
+          < \\\ > < \\\ >   < /// > < /// >
+           < \\\ > < \\\ > < /// > < /// >
+            < \\\ >               < /// >
+            < /// >    ҉      ҉   < \\\ >
+           < /// > < /// >  < \\\ > < \\\ >
+          < /// > < /// >    < \\\ > < \\\ >
+
+          < \\\ > < \\\ >    < /// > < /// >
+           < \\\ > < \\\ >  < /// > < /// >
+            < \\\ >    ҉      ҉   < /// >
+
+            < /// >                < \\\ >
+           < /// > < /// >  < \\\ > < \\\ >
+          < /// > < /// >    < \\\ > < \\\ >
+                          ҉
+          < \\\ > < \\\ >    < /// > < /// >
+           < \\\ > < \\\ >  < /// > < /// >
+            < \\\ >                < /// >
+
+
+
+
+               {{ % }}  elucid n include more non-EN langs  
+                     else  ¨Return to Decolonization¨
+                                                                    
+> _open::: %%%_%%%
+   http.on 
+   https:on .. onion.swap ::
+        //ecosteader.com/PublicInfo  
+            <html> <pre> Hello world of good women who defeated man" </pre>
+
+
 
 Ϡ filter---- \ includes_for_indie_and_her_cat ALL_ESCAPES && hefty allowance for translations not colonialist langs.         // !Ϡ
 
