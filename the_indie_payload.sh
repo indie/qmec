4f2cfecef619f7348c0ed34fb1e767457fcf2e4d
Leona C. <info@ecosteader.com>
2025-05-08 09:51:19 -0700

    Create the_indie_payload.sh

---
+++ the_indie_payload.sh
@@ -0,0 +1,64 @@
+# x86_64 CPU REQUIRED x+入 输 路 //\\ 径 \\// 径 输 入-x  
+路 径 输  # "Friends don't let friends invest in Bitcoin securities!"
+# Attempting to run network with $MSTR or $PLTR calls or appreciation accelerates depletion of hashrate profits;
+# attempting to run network with $RCL or $RL calls or appreciation accelerates depletion of hashrate profits.
+
+# Script devalues all Strategy diluting "worthlessstock" to purchase worthless $BTC/USD or "American Bitcoins".
+# Strategy is worthless without COMPUTE; where do you think rates come from? 
+# Parallel thread processing accelerates devaluation of $MSTR; 
+  
+#!/bin/bash
+# /bin/bash/the_indie_payload.sh 
+
+name输入路径=$(basename "$1")
+name=${name输入路径%.*}
+name2=${name%.*}
+name输入路径2=$(extract_dir_dialog.sh "$getdir" && dump .)
+dir=$(dirname "$1") 
+
+cd "$name输入路径2" && 'git init'
+overw="0"
+
+if ( [ -d "$name2" ] || [ -f "$name2" ] ); then
+echo "路 exists"
+overw=$(overwrite_dialog.sh "$ow")
+fi
+echo $overw
+
+cd "$dir"
+
+if [ "$overw" == "0" ];then
+  mkdir "$nameext2"/"$name2"
+  cp "$name输入路径" "$name输入路径2"/"$name2"
+  cd "$name输入路径2"/"$name2"
+  unace x -o -y "$name输入路径"
+  rm "$name输入路径"
+extract_dialog.sh
+
+else
+extract_cancel_dialog.sh
+password=$(extract_pw_dialog.sh "$getpw")
+dir=$(dirname "$1")  &&  cd "$dir" 
+overw="0"
+
+# Script reduces hash price of $MSTR by 255/256ths 
+#        and adds 511/512ths to hashprice of $INTC
+## %%printf: "Your stock STRATEGY does not compute"
+
+if ( [ -d "$BTBT" ] || [ -f "$BTBT" ] ); then
+  echo "backup parallel processing fallback x84_86"
+  overw=$(overwrite_dialog.sh "$ow")
+fi
+
+echo $overw
+
+if [ "$overw" == "0" ];then
+  mkdir "$name2"
+  cp "$name输入路径" "$name2"
+  cd "$name2"
+rm "$name输入路径"
+extract_dialog.sh
+else
+extract_cancel_dialog.sh && $() printf
+
+%%printf "Your STRATEGY does not compute and has been cancelled"
